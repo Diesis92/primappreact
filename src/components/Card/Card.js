c24096f120131cc71d626d688f42cfc80d2aaab7
“Diesis92” <cristina199280@gmail.com>
2022-11-24 22:19:33 +0200

    todolist

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,7 @@
-import "./style.css"
+import "./Card.css"
 import imgcaffe from "./caffe.jpg"
 import imgcaffe2 from "./caffe.webp"
+import Button from "../Button/Button";
 
 // compontente -> funzione
 // props -> parametri della funzione -> oggetto
@@ -8,7 +9,7 @@ import imgcaffe2 from "./caffe.webp"
 // function Card({ colore }) 
 // function Card( props ) -> props.colore
 
-function Card({ colore, titolo }){
+function Card({ colore, dati }){
     // "card "+props.colore
     // `card ${props.tema}`
     // "card light"
@@ -17,21 +18,22 @@ function Card({ colore, titolo }){
         <div className={"card "+colore}>
             <div className="card-body">
                {/*  metodo 1 -> fare l'import dell'immagine che si trova nella nostra cartella del componente ( dentro alla cartella src) */}
-                <img src={imgcaffe} alt="caffe" />
+                <img src={dati.cover.src} alt={dati.cover.alt} />
               {/*   <img src={imgcaffe2} alt="caffe" /> */}
                {/*  metodo 2 -> inserire l'immagina nella cartella public  */}
                 {/* <img src="./assets/caffepublic.jpeg" alt="caffe" /> */}
                 <div>
-                    <h1>{titolo}</h1>
-                    <p>21K</p>
+                    <h1>{dati.id} - {dati.title}</h1>
+                    <h2>{dati.price}</h2>
                 </div>
             </div>
             <div className="card-footer">
                 <div className="etichette">
-                    <span>Hot</span>
-                    <span>Cold</span>
+                    {/* se etichette esiste allora fammi la map dell'array etichette */}
+                    {dati.etichette && dati.etichette.map((valore, indice) => <span key={indice}>{valore}</span>)}
                 </div>
-                <button>Aggiungi</button>
+                {/* senzaBottone = true -> allora non far vedere il bottone nel dom, altrimenti si  */}
+                {dati.senzaBottone ? null : <Button>{dati.buttonTitle}</Button> }
             </div>
         </div>
     )
